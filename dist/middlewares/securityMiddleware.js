abf2a69b7b5bf71c64e99014fde1cbe6e634dcc4
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-09-16 00:53:12 +0500

    Adding dist to .gitignore

--- dist/middlewares/securityMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/middlewares/securityMiddleware.ts
-const helmet_1 = __importDefault(require("helmet"));
-const securityMiddleware = (0, helmet_1.default)();
-exports.default = securityMiddleware;
